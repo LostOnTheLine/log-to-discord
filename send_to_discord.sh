f130e84067db20f832eb3a1eae4113e245c860d9
LostOnTheLine <60684934+LostOnTheLine@users.noreply.github.com>
2023-03-19 23:51:09 -0700

    Add files via upload

--- send_to_discord.sh
+++ send_to_discord.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-tail -f "$LOG_FILE" | while read line; do
-    if [[ -n "${FILTER_BY_WORD}" ]] && ! echo "${line}" | grep -iq "${FILTER_BY_WORD}"; then
-        continue
-    fi
+IFS=',' read -ra files <<< "$LOG_FILE"
+tail -n0 -f ${files[@]} | while read line; do
+  if [[ ! -z "${FILTER_BY_WORD}" ]] && ! echo "${line}" | grep -iq "${FILTER_BY_WORD}"; then
+    continue
+  fi
     echo "$line" && \
     curl --silent --fail -X POST -H 'Content-Type: application/json' -d "{\"content\":\"$line\"}" ${DISCORD_WEBHOOK}
 done
